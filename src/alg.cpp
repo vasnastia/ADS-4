0bb36a2f28a7f77a0959163e6355654b273d6bf2
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-03-13 20:12:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,35 +19,35 @@ int countPairs2(int *arr, int len, int value) {
     if (arr[i] * 2 > value) break;
     for (int j = len - 1; j > i; j--) {
       if (arr[i] + arr[j] > value) continue;
-	    if (arr[i] + arr[j] < value) break;
-			if (arr[i] + arr[j] == value) {
-				count++;
-			}
-		}
-	}
-	return count;
+      if (arr[i] + arr[j] < value) break;
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
   int count = 0;
-	for (int i = 0; i < len - 1; i++) {
-		int num = value - arr[i];
-		int l = i + 1, r = len - 1;
-		while (l < r) {
-			int m = (l + r) / 2;
-			if (arr[m] < num) {
-				l = m + 1;
-			} else {
-				r = m;
-			}
-		}
-		if (arr[l] == num) {
-			while (arr[l] == num) {
-				count++;
-				l++;
-			}
-		}
-	}
-	return count;
+  for (int i = 0; i < len - 1; i++) {
+    int num = value - arr[i];
+    int l = i + 1, r = len - 1;
+    while (l < r) {
+      int m = (l + r) / 2;
+      if (arr[m] < num) {
+        l = m + 1;
+      } else {
+        r = m;
+      }
+    }
+    if (arr[l] == num) {
+      while (arr[l] == num) {
+        count++;
+        l++;
+      }
+    }
+  }
+  return count;
 }
