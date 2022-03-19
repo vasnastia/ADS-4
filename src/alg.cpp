24b39238387ef15c0b7b20d77ce7ebe18a8053e1
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-03-19 13:17:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,7 +17,7 @@ int countPairs2(int *arr, int len, int value) {
   int counter = 0;
   for (int k = 0; k < len - 1; k++) {
     if (arr[k] * 2 > value) break;
-    for (int kk = len - 1; kk > i; kk--) {
+    for (int kk = len - 1; kk > k; kk--) {
       if (arr[k] + arr[kk] > value) continue;
       if (arr[k] + arr[kk] < value) break;
       if (arr[k] + arr[kk] == value) {
@@ -28,10 +28,10 @@ int countPairs2(int *arr, int len, int value) {
   return counter;
 }
 
-int cbinsearch(int *arr, int size,int critik, int value) {
+int cbinsearch(int *arr, int size, int critik, int value) {
   int count = 0, mm = critik, rr = size - 1, middle = 0;
   while (mm <= rr) {
-    middle = mm + (rr - l) / 2;
+    middle = mm + (rr - mm) / 2;
     if (arr[middle] == value && middle != critik) {
       counter++;
       int mm1 = middle, mm2 = middle;
@@ -55,7 +55,7 @@ int countPairs3(int *arr, int len, int value) {
   return 0;
   int counter = 0;
   for (int k = 0; k < len - 1; k++) {
-    if (arr[k] <= value - arr[k] {
+    if (arr[k] <= value - arr[k]) {
       counter += cbinsearch(arr, len, k, value - arr[k]);
     }
   }
